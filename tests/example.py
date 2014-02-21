63494bee7a76406282890cf4c286213e0708754f
utilisateur1 <reynaud_francois@hotmail.com>
2014-02-21 12:39:44 +0100

    introduction d'une boucle

--- tests/example.py
+++ tests/example.py
@@ -8,7 +8,7 @@ openfisca_france.init_country()
 from openfisca_core.simulations import ScenarioSimulation
 
 
-def case_study (year = 2013) :
+def case_study(year = 2013) :
 #simulation est une classe , Scenariosimulation  est une instance, on a instancier "simulation"
 #on a mainteant fait simulation.set_congif pour modifier les attributs. on lui a dit de prendre l'année 2013
 #avec year=year. reforme = true. Nmen est le nombre de ménage. Maxrev est le salaire imposable de la première personne déclaré.
@@ -17,13 +17,27 @@ def case_study (year = 2013) :
     simulation = ScenarioSimulation()
     simulation.set_config(year = year,
                           nmen = 2,
-                          maxrev = 10000,
+                          maxrev = 500000,
                           x_axis= 'sali')
     simulation.set_param()
     df = simulation.get_results_dataframe()
     print df.to_string()
     
+    
+
+def couple_38ans_avec_3enfants(year = 2013):
+    simulation = ScenarioSimulation()
+    simulation.set_config(year=year,
+					nmen=1,
+					maxrev = 500000,
+                          x_axis= 'sali',
+                          )
+    print simulation.scenario
+    simulation.scenario.addIndiv(1, datetime(1975,1,1).date(), 'conj', 'part')
+    simulation.scenario.addIndiv(2, datetime(2000,1,1).date(), 'pac1', 'enf1')
+    simulation.scenario.addIndiv(3, datetime(2005,1,1).date(), 'pac2', 'enf2')
+    simulation.scenario.addIndiv(4, datetime(2010,1,1).date(), 'pac3', 'enf3')
 
 if __name__ == '__main__':
-
- case_study()
+    case_study()
+    couple_38ans_avec_3enfants()
